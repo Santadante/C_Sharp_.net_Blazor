8cbb2f5bcf3581610bf4186087269bbfb229d51d
David Santacruz <santacruzdl@gmail.com>
2022-08-07 20:01:15 +0200

    First App in C#

---
+++ C#/_001_ConsolaApp/_001_ConsolaApp/Car.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_ConsolaApp {
+    internal class Car {
+        public string PetName { get; set; }
+        public string Make { get; set; }
+    }
+}
---
+++ C#/_001_ConsolaApp/_001_ConsolaApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// Set up console UI (CUI)
+Console.Title = "My first App";
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.BackgroundColor = ConsoleColor.Green;
+
+Console.WriteLine("**********************************");
+Console.WriteLine("*** Welcome to my first App ;) ***");
+Console.WriteLine("**********************************");
+
+Console.BackgroundColor = ConsoleColor.Black;
+
+// Wait for Enter key to be pressed
+Console.Read();
